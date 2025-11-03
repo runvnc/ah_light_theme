be3a29afe78879fe8aad8d51c726adcb73f1ea3d
Jason Livesay <runvnc@gmail.com>
2025-11-02 22:11:45 -0600

    init

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ah_light_theme",
+    version="1.0.0",
+    packages=find_packages(where="src"),
+    package_dir={"":"src"},
+    include_package_data=True,
+    package_data={
+        'ah_light_theme': [
+            'static/js/*.css'.
+            'inject/*.jinja2',
+        ],
+    },
+    install_requires=[],
+)
